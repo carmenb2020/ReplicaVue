a455d78ec2cc25b8c571d3a439fadda5e867fdbe
Leolo Flórez <116220158+LeoloFlrz@users.noreply.github.com>
2023-06-13 10:48:02 +0200

    13/06 1

---
+++ vue-project/src/components/HappyUsers.vue
@@ -0,0 +1,26 @@
+<script>
+  defineProps({
+    srcPath: {
+      type: String,
+      required: true
+    },
+
+    pMsg: {
+      type: String,
+      required: true
+    }
+  })
+</script>
+
+<template>
+      <div class="section-numbers__container section-numbers__users">
+        <img src="./assets/Images/usersIconFix.png" alt="users icon" />
+        <p class="section-numbers__container__number">231</p>
+        <p>Happy Users</p>
+      </div>   
+    
+</template>
+
+<style lang="css">
+    
+</style>
