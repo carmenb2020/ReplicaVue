85912abe63e1457d9a90ce7183d4a9b2f62000ad
carmenb2020 <carmenborjap@hotmail.com>
2023-06-14 17:51:42 +0200

    add section-numbers to Screens2View

--- vue-project/src/views/Screens2View.vue
+++ vue-project/src/views/Screens2View.vue
@@ -1,7 +1,20 @@
 <script>
     import PinkButton from '../components/PinkButton.vue'
+    import HappyUsers from '../components/HappyUsers.vue'
 </script>
 <template>
+    <section class="section-numbers">
+        <HappyUsers propClass="section-numbers__container section-numbers__users" srcPath="src/assets/Images/usersIconFix.png" pCount="350" pCountClass="section-numbers__container__number" pMsg="Happy Users"/>
+      
+        <HappyUsers propClass="section-numbers__container section-numbers__issues"  srcPath="src/assets/Images/keyIconFix.png" pCount="137" pCountClass="section-numbers__container__number" pMsg="Issues Solved"/>
+
+        <HappyUsers propClass="section-numbers__container section-numbers__reviews" srcPath="src/assets/Images/gearIconFix.png" pCount="233" pCountClass="section-numbers__container__number" pMsg="Good Reviews"/>
+
+        <HappyUsers propClass="section-numbers__container section-numbers__case" srcPath="src/assets/Images/messageIconFix.png" pCount="127" pCountClass="section-numbers__container__number" pMsg="Case Studies"/>
+
+        <HappyUsers propClass="section-numbers__container section-numbers__orders" srcPath="src/assets/Images/rocketIconFix.png" pCount="211" pCountClass="section-numbers__container__number" pMsg="Orders Received"/>
+    </section>
+      
     <section class="section-bottom">
         <div class="section-bottom__image">
             <img id="phone-image-bottom" src="./assets/Images/download-iphone.png" alt="imagen fondo 4" />
@@ -22,6 +35,13 @@
     </section>
 </template>
 <style>
+    .section-numbers {
+        display: flex;
+        justify-content: center;
+        background-color: #e8eff6;
+        padding: 60px;
+    }
+    
     .section-bottom {
     padding-top: 100px;
     padding-bottom: 100px;
